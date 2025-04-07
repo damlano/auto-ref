36266e7332bea221389bd6040d4810cdfaf88ec2
damlano <no@no>
2025-04-08 00:33:03 +0200

    edited main.py

--- main.py
+++ main.py
@@ -16,11 +16,9 @@ print(BEATMAP_IDS)
 joined_users = []
 mutli_id = 0
 Current_Beatmap_Index = 0
-# to do
-# add json for ocnfig fo beatmaps etc
-leftduringmap = []
-mapstarttime = 0
-mapendtime = 0
+aborted = False
+votes_aborted = 0
+matchongoing = False
 
 logname = "logging_for_osu_multiplayer_bot.txt"
 
@@ -42,8 +40,10 @@ def start_multi(connection):
 
 def parse_pubmsg(connection, event):
     try:
+        global mutli_id, joined_users, Current_Beatmap_Index, aborted, votes_aborted, matchongoing
         sender = event.source
         message = event.arguments[0]
+        msgtarget = event.target
         target = str(message).split(" joined in slot")[0]
         target = target.replace(" ", "_")
 
@@ -55,15 +55,30 @@ def parse_pubmsg(connection, event):
              connection.privmsg(f'#mp_{mutli_id}', f"!mp kick {target}")
 
         if set(joined_users) == set(EXPECTED_USERS) and "joined in slot" in message:
-            connection.privmsg(f'#mp_{mutli_id}', "Please ready up everyone automatically starting in 60 seconds")
+            connection.privmsg(f'#mp_{mutli_id}', f"Please ready up everyone automatically starting in {config.time_between_maps} seconds")
             connection.privmsg(f'#mp_{mutli_id}', f"!mp timer {config.time_between_maps}")
+            connection.privmsg(f'#mp_{mutli_id}', f"Each map everyone gets 1 time of aborting the map if half the lobby aborts the map will be auto-aborted")
 
-        if set(joined_users) == set(EXPECTED_USERS) and "All players are ready" in message:
+        if set(joined_users) == set(EXPECTED_USERS) and "All players are ready" in message and msgtarget == f"#mp_{mutli_id}":
+            matchongoing = True
             connection.privmsg(f'#mp_{mutli_id}', "!mp timer stop")
             connection.privmsg(f'#mp_{mutli_id}', "!mp start")
 
-        if "banchobot" in str(sender).lower() and "Countdown finished" in message:
+        if "banchobot" in str(sender).lower() and "Countdown finished" in message and msgtarget == f"#mp_{mutli_id}":
+            matchongoing = True
             connection.privmsg(f'#mp_{mutli_id}', "!mp start")
+
+
+        sender =  str(sender).split("!")[0]
+        if sender in EXPECTED_USERS and str(message).startswith(".abort") and msgtarget == f"#mp_{mutli_id}":
+                votes_aborted = votes_aborted+1
+                if votes_aborted >= len(EXPECTED_USERS)//2 and aborted == False and matchongoing == True:
+                    votes_aborted = 0
+                    aborted = True
+                    connection.privmsg(f'#mp_{mutli_id}', f"!mp abort")
+                    connection.privmsg(f'#mp_{mutli_id}', f"!mp timer 120")
+                    print("match aborted")
+                    matchongoing = False
             
         if "banchobot" in str(sender).lower() and "The match has finished" in message:
             global Current_Beatmap_Index
@@ -74,8 +89,11 @@ def parse_pubmsg(connection, event):
                 connection.privmsg(f'#mp_{mutli_id}', f"!mp timer {config.time_between_maps}")
                 connection.privmsg(f'#mp_{mutli_id}', "everyone ready up for next map!")
                 Current_Beatmap_Index += 1
+                matchongoing = False
+                aborted = False
             else:
                 connection.privmsg(f'#mp_{mutli_id}', "We reached the end of the beatmap list! Thank you for joining!!")
+                matchongoing = False
 
     except Exception as e:
         logger.error(f"Error processing privmsg: {str(e)}")
@@ -83,20 +101,19 @@ def parse_pubmsg(connection, event):
 
 def parse_privmsg(connection, event):
     try:
+        global mutli_id, joined_users, Current_Beatmap_Index, aborted, votes_aborted
         sender = event.source
         message = event.arguments[0]
         logger.debug(f"parse_privmsg logging: sender: {sender} message: {message} event: {event}")
         logger.debug(f"Received PRIVMSG: {message} from {sender}")
 
         if "banchobot" in str(sender).lower() and "Created the tournament match https://osu.ppy.sh/mp/" in message:
-            global mutli_id
             mutli_id = message.split('/')[-1].split()[0]
             if mutli_id != 0:
                 print(f"\nmulti id = {mutli_id}")
                 global Current_Beatmap_Index
                 connection.privmsg(f'#mp_{mutli_id}', f"!mp map {BEATMAP_IDS[Current_Beatmap_Index][0]} 0")
                 connection.privmsg(f'#mp_{mutli_id}', f"!mp timer {config.time_between_maps}")
-                time.sleep(2)
                 connection.privmsg(f'#mp_{mutli_id}', f"!mp set 0 3 {len(EXPECTED_USERS)}")
                 for i in EXPECTED_USERS:
                     connection.privmsg(f'#mp_{mutli_id}', f"!mp invite {i}")
