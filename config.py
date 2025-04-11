ad63bd8a2d2f941c938e978b9d7ae007f9cfe405
demi_nya <145002907+damlano@users.noreply.github.com>
2025-04-11 17:31:08 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,39 @@
+multiplayer_name = "name"
+time_between_maps = 120
+EXPECTED_USERS = [""]
+user_ammount_to_start = 10
+
+server = "irc.ppy.sh"
+user = "user"
+password = "password"
+port = 6667
+
+BEATMAP_IDS = [  # 0 = unused
+    (1783725, ""),  # NM1
+    (4525964, ""),  # NM2
+    (4091822, ""),  # NM3
+    (2142695, ""),  # NM4
+    (0, ""),        # NM5
+    (0, ""),        # NM6
+
+    (4074059, "HR"),  # HR1
+    (4619931, "HR"),  # HR2
+    (0, "HR"),        # HR3
+    (0, "HR"),        # HR4
+
+    (4147061, "HD"),  # HD1
+    (4377688, "HD"),  # HD2
+    (0, "HD"),        # HD3
+    (0, "HD"),        # HD4
+
+    (2449709, "DT"),  # DT1
+    (3912911, "DT"),  # DT2
+    (0, "DT"),        # DT3
+    (0, "DT"),        # DT4
+    
+    (0, "FreeMod"),        # FM1
+    (0, "FreeMod"),        # FM2
+    (0, "FreeMod"),        # FM3
+
+    (0, "TB"),        # TB1
+]
